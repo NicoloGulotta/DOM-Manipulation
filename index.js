7c3c2044165a9b6447fb33f29895eb1fc0000dc6
Nicolò Gulotta <Gulottanico1998@gmail.com>
2023-11-27 18:58:31 +0100

    finale

--- index.js
+++ index.js
@@ -20,8 +20,49 @@ function modificaFT() {
 modificaFT();
 
 function aggiungiCL() {
-    let addCl = document.getElementsById("link");
-  addCl.classList[0].add("Link");
+    let addCl = document.getElementsByClassName("link");
+    for (let i = 0; i < addCl.length; i++) {
+        addCl[i].classList.add("new");
 
+    }
 }
 aggiungiCL();
+
+function toggleImg() {
+    let removeImage = document.getElementsByTagName("img");
+    for (let img = 0; img < removeImage.length; img++) {
+        removeImage[img].classList.toggle("disappear");
+    }
+}
+removeImg()// funzione attiva ed img tolte 
+removeImg()// reinvocazione per riattivazione toggle e class presenti
+
+function imgSetting() {
+    let img = document.getElementsByClassName("disappear");
+    for (let index = 0; index < img.length; index++) {
+        img[index].style.display = "none"
+    }
+}
+imgSetting()
+
+function randomNumber(limitValue) {
+    let randomicNumber = Math.random();
+    randomicNumber = randomicNumber * limitValue;
+    randomicNumber = math.floor(randomicNumber);
+    return randomicNumber;
+}
+function randomicColor() {
+    let limitNum = 256;
+    let r = randomNumber(limitNum);
+    let g = randomNumber(limitNum);
+    let b = randomNumber(limitNum);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+
+}
+function changePriceColor() {
+    let price = document.getElementsByClassName("price");
+    for (let color = 0; color < price.length; color++){
+        price[color].style.color=randomicColor();
+    }
+}
+changePriceColor()
